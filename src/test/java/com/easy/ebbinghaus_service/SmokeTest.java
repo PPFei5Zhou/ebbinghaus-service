cdb51888bd0db63b6f946b555daa4bf66419d24c
PPFei5Zhou <pengfei_zhou555@163.com>
2022-03-14 20:55:31 +0800

    Create application-env.template.yml and remove environment configuration from application.yml

--- src/test/java/com/easy/ebbinghaus_service/SmokeTest.java
+++ src/test/java/com/easy/ebbinghaus_service/SmokeTest.java
@@ -1,9 +1,11 @@
 package com.easy.ebbinghaus_service;
 
+import com.easy.ebbinghaus_service.core.DatabaseTestConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -11,6 +13,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Import(DatabaseTestConfiguration.class)
 public class SmokeTest {
 
     @Autowired
